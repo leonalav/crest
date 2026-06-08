7d93ff171dba2cc9c000216ace7da5aba15143df
leonalav <duchuypm@gmail.com>
2026-06-08 10:56:37 +0700

    indent fix for cli prepare

--- src/crest/cli_prepare_manifest.py
+++ src/crest/cli_prepare_manifest.py
@@ -17,7 +17,7 @@ def tokenizer_meta(tokenizer) -> dict:
         vocab_size = int(len(tokenizer))
     except TypeError:
         vocab_size = int(getattr(tokenizer, "vocab_size", 0))
-     pad_id = getattr(tokenizer, "pad_token_id", None)
+    pad_id = getattr(tokenizer, "pad_token_id", None)
     eos_id = getattr(tokenizer, "eos_token_id", None)
     bos_id = getattr(tokenizer, "bos_token_id", None)
     if pad_id is None:
